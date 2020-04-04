fde7f18dbb729f231bcd3e00fbf5679c1f274cc3
Harsh Hajela <harsh.hajela@telensa.com>
2020-04-04 20:54:50 +0100

    checkin

--- src/test/java/com/hajela/testing/HelloWorldTest.java
+++ src/test/java/com/hajela/testing/HelloWorldTest.java
@@ -4,7 +4,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 class HelloWorldTest {
 
@@ -39,5 +46,16 @@ class HelloWorldTest {
     @AfterAll
     public static void afterClass() {
         System.out.println("AfterAll called...");
+    }
+
+    @DisplayName("Value source test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getData")
+    void paramTest(String val) {
+        System.out.println(val);
+    }
+
+    private static Stream<Arguments> getData(){
+        return Stream.of(Arguments.of("Return data from method!"));
     }
 }
